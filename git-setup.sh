6ccd422649cdfc061d4355094bf795bfe8acc249
juanbagnardi98 <juanibagnardi98@hotmail.com>
2026-04-17 13:38:00 -0300

    Create git-setup.sh

---
+++ git-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Inicializar repositorio Git
+git init
+
+# Agregar todos los archivos
+git add .
+
+# Hacer commit inicial
+git commit -m "Initial commit: Proyecto Hogwarts - web estática con 5 páginas, SASS, Bootstrap y Animate.css"
+
+# Agregar remoto (reemplaza TU_USUARIO y TU_REPO con los valores correctos)
+# git remote add origin https://github.com/TU_USUARIO/TU_REPO.git
+
+# Cambiar rama a main (opcional, depende de GitHub)
+# git branch -M main
+
+# Hacer push al repositorio remoto
+# git push -u origin main
